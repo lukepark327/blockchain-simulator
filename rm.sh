d6e69d04e3259932a0ab6b7518b7bcbdeea8e925
twodude <twodude@naver.com>
2019-02-08 20:39:20 +0900

    add preconditions.sh

--- rm.sh
+++ rm.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-rm -rf onechain/node_modules/
+rm -rf node_modules/
+
 rm -rf onechain/wallet/
 rm onechain/nohup.out
 
-rm -rf master/node_modules/
 rm -rf master/wallet/
 rm master/nohup.out
+
+rm -rf mallcious/wallet/
+rm mallcious/nohup.out
 
 rm table.json
