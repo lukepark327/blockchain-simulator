958b248744ac0a0a6677dee94207fafcb02392b0
twodude <twodude@naver.com>
2019-02-10 15:11:34 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # nohup python3 main.py --nodes=24
-python3 main.py --nodes=120 --neighbors=8 --prop_delay_avg=5000 --prop_delay_std=2000 --sleep=10
+nohup python3 main.py --nodes=400 --neighbors=8 --prop_delay_avg=5000 --prop_delay_std=2000 --sleep=60
